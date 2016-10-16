3f053ce521e98f12f806ebe25e5c45e875367a8e
owner90 <owner90@owner90-PC>
2016-10-16 16:17:35 +0300

    faktoriel

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);	
 
+		System.out.println("Input two number: ");
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int fa = 1;
+		
+		for(int i=1;i<=a; i++){
+			fa=fa*i;
+		}
+		int fb=1;
+		
+		for(int i=1;i<=b;i++){
+			fb=fb*i;
+			
+		}
+		System.out.println((double)fa/(double)fb);
+		
 	}
-
+	
 }
